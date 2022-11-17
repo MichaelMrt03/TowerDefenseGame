cff982bd1b0210a46d84737e666fa2d7e38977d8
Michael <Michael.Martynov@yahoo.com>
2022-11-17 19:10:41 +0100

    Map hinzugefügt und GLOOP grundlagen

--- Spielverwalter.java
+++ Spielverwalter.java
@@ -1,4 +1,18 @@
 import GLOOP.*;
 public class Spielverwalter {
-    
+
+    static GLKamera cam;
+    GLLicht licht;
+    Map map;
+    public Spielverwalter(){
+         cam = new GLSchwenkkamera();
+        new Map();
+        new GLLicht();
+       spielerperspektive();
+    }
+
+    public static void spielerperspektive() {
+        cam.verschiebe(0,1200,-300);
+        cam.setzeBlickpunkt(0,0,0);    
+    }
 }
